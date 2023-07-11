f11108f22d975879c96b31864ab3e33780de86c6
Fouzia-Zub <fouziazubedi.23@gmail.com>
2023-07-11 23:14:54 +0530

    changed return_url

--- src/components/checkoutForm/CheckoutForm.js
+++ src/components/checkoutForm/CheckoutForm.js
@@ -93,7 +93,8 @@ const CheckoutForm = () => {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000/checkout-success",
+        // http://localhost:3000/checkout-success
+        return_url: "https://e-commerce-fouzia-zub.vercel.app/checkout-success",
       },
       redirect: "if_required" 
     })
